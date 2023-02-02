aa9e04bd1aacc2ba44de85751e71ef4c524eeecf
shawn <shawn.montgomery@verisimlife.com>
2023-02-02 16:03:54 -0500

    Update version

--- docker_app/client/setup.py
+++ docker_app/client/setup.py
@@ -8,17 +8,19 @@ from setuptools import setup, find_packages
 setup(
     name = 'vsl_poc_movie_client',
 
-    version = '1.0.0',
+    version = '1.0.1',
 
     description = 'An sample Python package',
     license = 'MIT',
     package_dir = {"": "vsl_poc_movie_client"},
+    include_package_data=True,
     packages = find_packages(
         # includes dirs with __init__.py
         where="vsl_poc_movie_client",
-        include="*",
-        exclude=[]
+        # include="*",
+        exclude=["tests"]
     ),
+    py_modules=[],
     python_requires = '>=3.6',
     install_requires=[
         # Loosely defined dependencies
